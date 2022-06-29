41133163486e44eb7a85f0ea0285e1488418672c
adrianpura <pura.adriano@gmail.com>
2022-06-29 15:42:53 +0800

    local storage
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,20 +1,13 @@
-const notes = [
-    {
-        title: 'Note 1',
-        body: 'Body 1'
-    },
-    {
-        title: 'Note 2',
-        body: 'Body 2'
-    },
-    {
-        title: 'Note 3',
-        body: 'Body 3'
-    },
-]
+let notes = []
 
 const filters = {
     searchText: ''
+}
+
+//check existing data
+const notesJSON = localStorage.getItem('notes')
+if (notesJSON !== null) {
+    notes=JSON.parse(notesJSON)
 }
 
 const renderNotes = function (notes, filter) {
@@ -26,7 +19,11 @@ const renderNotes = function (notes, filter) {
 
     filteredNotes.forEach(function (note) {
         const noteEl = document.createElement('p')
-        noteEl.textContent = note.title
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title
+        } else {
+            noteEl.textContent = 'Unnamed note'
+        }
         document.querySelector('#notes').appendChild(noteEl)
     })
 }
@@ -35,7 +32,12 @@ renderNotes(notes, filters)
 
 
 document.querySelector('#create-note').addEventListener('click', function (e) {
-    e.target.textContent = 'the button was click'
+    notes.push({
+        title: '',
+        body: ''
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    renderNotes(notes, filters)
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
--- todo-app/todo.js
+++ todo-app/todo.js
@@ -1,29 +1,14 @@
-const todos = [
-    {
-        text: 'Clean the Kitchen',
-        completed: true
-    },
-    {
-        text: 'Book Flights',
-        completed: false
-    },
-    {
-        text: 'Walk the dog',
-        completed: true
-    },
-    {
-        text: 'Research Museums',
-        completed: false
-    },
-    {
-        text: 'finish this course',
-        completed: true
-    }
-]
+let todos = []
 
 const filters = {
     searchText: '',
     hideCompleted: false
+}
+
+//check existing data
+const todosJSON = localStorage.getItem('todos')
+if (todosJSON !== null) {
+    todos = JSON.parse(todosJSON)
 }
 
 const renderTodo = function (todos, filters) {
@@ -46,16 +31,14 @@ const renderTodo = function (todos, filters) {
 
     filteredTodos.forEach(function (todo) {
         const p = document.createElement('p')
-        p.textContent = todo.text
+        if (todo.text.length > 0) {
+            p.textContent = todo.text
+        } else {
+            p.textContent = 'unnamed todo'
+        }
+
         document.querySelector('#todos').appendChild(p)
     })
-
-
-    // filteredTodos.forEach(function (todo) {
-    //     const todoEl = document.createElement('p')
-    //     todoEl.textContent = todo.text
-    //     document.querySelector('#todos').appendChild(todoEl)
-    // })
 }
 renderTodo(todos, filters)
 
@@ -70,6 +53,7 @@ document.querySelector('#todo-form').addEventListener('submit', function (e) {
         text: e.target.elements.todoText.value,
         completed: false
     })
+    localStorage.setItem('todos', JSON.stringify(todos))
     renderTodo(todos, filters)
     e.target.elements.todoText.value = ''
 })
