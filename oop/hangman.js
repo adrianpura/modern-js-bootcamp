aef4265a673915b8c322548b299ed0ac18687be4
adrianpura <pura.adriano@gmail.com>
2022-07-07 23:48:42 +0800

    hangman
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,11 +1,26 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word
+const Hangman = function (word = [], remainingGuesses) {
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c','e']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    });
+
+    return puzzle
 }
 
 
-const hangman1 = new Hangman('any', 2)
-console.log(hangman1)
+const hangman1 = new Hangman('Cat', 2)
+console.log(hangman1.getPuzzle())
 
-const hangman2 = new Hangman('any1', 22)
-console.log(hangman2)
+const hangman2 = new Hangman('New Jersey', 22)
+console.log(hangman2.getPuzzle())
