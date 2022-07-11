6b1e9a805983e063c85b6e5df891618b0ec99680
adrianpura <pura.adriano@gmail.com>
2022-07-11 23:49:07 +0800

    http
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- hangman/app.js
+++ hangman/app.js
@@ -1,3 +1,5 @@
+
+
 const puzzleEl = document.querySelector('#puzzle')
 const guessesEl = document.querySelector('#guesses')
 
@@ -6,9 +8,25 @@ const hangman1 = new Hangman('Car Parts', 2)
 puzzleEl.textContent = hangman1.puzzle
 guessesEl.textContent = hangman1.statusMessage
 
-window.addEventListener('keypress', function (e) {
+window.addEventListener('keypress', (e) => {
     const guess = String.fromCharCode(e.charCode)
     hangman1.makeGuess(guess)
     puzzleEl.textContent = hangman1.puzzle
     guessesEl.textContent = hangman1.statusMessage
 })
+
+
+
+//making an http request
+const request = new XMLHttpRequest()
+request.addEventListener('readystatechange', (e) => {
+    if (e.target.readyState === 4 && e.target.status === 200) {
+        const data = JSON.parse(e.target.responseText)
+        console.log(data)
+    } else if (e.target.readyState === 4) {
+        console.log('error')
+    }
+})
+request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
+request.send()
+// console.log(request.responseText)
