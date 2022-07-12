f11825cc9279aeb602be21ff527fb53f26bdc87b
adrianpura <pura.adriano@gmail.com>
2022-07-12 22:02:09 +0800

    callback function
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- hangman/app.js
+++ hangman/app.js
@@ -16,17 +16,34 @@ window.addEventListener('keypress', (e) => {
 })
 
 
-
-//making an http request
-const request = new XMLHttpRequest()
-request.addEventListener('readystatechange', (e) => {
-    if (e.target.readyState === 4 && e.target.status === 200) {
-        const data = JSON.parse(e.target.responseText)
-        console.log(data)
-    } else if (e.target.readyState === 4) {
-        console.log('error')
+getPuzzle((error, puzzle) => {
+    if (error) {
+        console.log(`Error: ${error}`)
+    } else {
+        console.log(puzzle)
     }
 })
-request.open('GET', 'http://puzzle.mead.io/puzzle?wordCount=3')
-request.send()
-// console.log(request.responseText)
+
+
+
+// //making an http request
+
+// // console.log(request.responseText)
+
+// const countryCode = 'PH'
+// const countryRequest = new XMLHttpRequest()
+
+// countryRequest.addEventListener('readystatechange', (e) => {
+
+//     if (e.target.readyState === 4 && e.target.status === 200) {
+//         const data = JSON.parse(e.target.responseText)
+//         const country = data.find((country) => country.alpha2Code === countryCode)
+//         console.log(country.name)
+//     } else if (e.target.readyState === 4) {
+//         console.log('error')
+//     }
+// })
+
+// countryRequest.open('GET', 'http://restcountries.com/v2/all', true)
+// countryRequest.send()
+// // console.log(request.responseText)
