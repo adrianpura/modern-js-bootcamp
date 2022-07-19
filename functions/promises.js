be5701d7fcdac8573518627b941acd8b048ffbd3
adrianpura <pura.adriano@gmail.com>
2022-07-19 17:46:02 +0800

    promise chaining
    Signed-off-by: adrianpura <pura.adriano@gmail.com>

--- functions/promises.js
+++ functions/promises.js
@@ -1,42 +1,53 @@
 //callback
-const getDataCallback = (callback) => {
+const getDataCallback = (num, callback) => {
     setTimeout(() => {
-        callback('this is the callback error', undefined)
-        callback('this is the callback error', undefined)
+        if (typeof num === 'number') {
+            callback(undefined, num * 2)
+        } else {
+            callback('number must be provided')
+        }
     }, 2000);
 }
 
 
-getDataCallback((err, data) => {
+getDataCallback(2, (err, data) => {
     if (err) {
         console.log(err)
     } else {
-        console.log(data)
+        getDataCallback(data, (err, data) => {
+            if (err) {
+                console.log('err: ', err);
+            } else {
+                console.log('data: ', data);
+            }
+        })
     }
 })
 
 //promise 
 
-const getDataPromise = (data) => new Promise((resolve, reject) => {
+const getDataPromise = (num) => new Promise((resolve, reject) => {
     setTimeout(() => {
-        resolve(`this is the promise data ${data}`)
-        // reject('this is the promise error')
-        // reject('this is the promise error')
+        typeof num === 'number' ? resolve(num * 2) : reject('number must be provided')
     }, 2000);
 })
 
-const myPromise = getDataPromise(123)
-
-myPromise.then((data) => {
-    console.log(data)
+getDataPromise(2).then((data) => {
+    getDataPromise(data).then((data) => {
+        console.log('promise data: ', data);
+    }, (err) => {
+        console.log('err: ', err);
+    })
 }, (err) => {
-    console.log(err)
+    console.log('err: ', err);
 })
 
-
-myPromise.then((data) => {
-    console.log(data)
-}, (err) => {
-    console.log(err)
+getDataPromise('10').then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    return getDataPromise(data)
+}).then((data) => {
+    console.log('data: ', data);
+}).catch((err) => {
+    console.log('err: ', err);
 })
-
--- hangman/app.js
+++ hangman/app.js
@@ -15,25 +15,29 @@ window.addEventListener('keypress', (e) => {
     guessesEl.textContent = hangman1.statusMessage
 })
 
-getPuzzle('1').then((puzzle) => {
-    console.log(puzzle)
-}, (err) => {
-    console.log('err: ', err);
-})
-
-const countryCode = 'PH'
-// getCountryDetails(countryCode, (error, countryDetails) => {
-//     if (error) {
-//         console.log(`Error: ${error}`)
-//     } else {
-//         console.log(countryDetails.name)
-//     }
+// getPuzzle('1').then((puzzle) => {
+//     console.log(puzzle)
+// }, (err) => {
+//     console.log('err: ', err);
 // })
 
-getCountryDetails(countryCode).then((countryDetails) => {
-    console.log('countryDetails: ', countryDetails);
-}, (err) => {
-    console.log('err: ', err);
+// const countryCode = 'PH'
+// getCountryDetails(countryCode).then((countryDetails) => {
+//     console.log('countryDetails: ', countryDetails.name);
+// }, (err) => {
+//     console.log('err: ', err);
 
+// })
+
+fetch('https://cors-anywhere.herokuapp.com/http://puzzle.mead.io/puzzle', {}).then((response) => {
+    if (response.status === 200) {
+        return response.json()
+    } else {
+        throw new Error('Unable to fetch the puzzle')
+    }
+}).then((data) => {
+    console.log('data: ', data.puzzle);
+}).catch((error) => {
+    console.log('error: ', error);
 })
 
